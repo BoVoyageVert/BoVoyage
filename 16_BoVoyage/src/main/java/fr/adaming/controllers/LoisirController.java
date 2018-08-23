7a09032e28a9daadd10ac84d748c89b07c3039f7
Adaming <Adaming@192.168.1.148>
2018-08-23 14:02:40 +0200

    claire: commit

--- 16_BoVoyage/src/main/java/fr/adaming/controllers/LoisirController.java
+++ 16_BoVoyage/src/main/java/fr/adaming/controllers/LoisirController.java
@@ -5,14 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import fr.adaming.model.Etudiant;
 import fr.adaming.model.Loisir;
 import fr.adaming.service.ILoisirService;
 
